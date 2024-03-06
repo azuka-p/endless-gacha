944369202ffa3365ced8f973b862333b403ed24f
azuka-p <rad.azka.16@gmail.com>
2024-03-07 00:01:16 +0700

    Try making character

---
+++ scenes/characters/Character.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name Character
+
+export var name: String = ""
+export var base: Texture
+export var hair: Texture
+
+var clothes_top: Texture = preload("res://assets/GandalfHardcore Character Asset Pack/Female Clothing/Corset v2.png")
+var clothes_mid: Texture = preload("res://assets/GandalfHardcore Character Asset Pack/Female Clothing/Skirt.png")
+var clothes_bot: Texture = preload("res://assets/GandalfHardcore Character Asset Pack/Female Clothing/Boots.png")
+var hand: Texture = preload("res://assets/GandalfHardcore Character Asset Pack/Female Hand/Female Sword.png")
