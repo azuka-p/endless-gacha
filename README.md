25071c28861915ad67701419723f171be9b4a2ef
azuka-p <rad.azka.16@gmail.com>
2024-03-07 23:10:53 +0700

    Implement gacha screen

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ https://leohpaz.itch.io/rpg-essentials-sfx-free
 https://fontmeme.com/pixel-fonts/
 https://github.com/Ertain/Random-Name-Generator
 https://kol.coldfront.net/thekolwiki/index.php/Advancement#Advancement_Requirements_Overview
+https://www.youtube.com/watch?v=X3J0fSodKgs
+https://www.youtube.com/watch?v=AkKywZDJNeE
+https://forum.godotengine.org/t/how-to-add-frames-from-a-sprite-sheet-in-code/5230
+https://gamedevacademy.org/spriteframes-in-godot-complete-guide/
+https://gamedevacademy.org/atlastexture-in-godot-complete-guide/
+https://docs.godotengine.org/en/3.5/
---
+++ scenes/gacha/GachaScreen.gd
@@ -0,0 +1,23 @@
+extends Control
+
+const Character = preload("res://scenes/characters/Character.tscn")
+
+
+func _on_Summon10_pressed():
+	var all = []
+	$NinePatchRect.visible = true
+	$VBoxContainer.visible = false
+	for i in range(10):
+		var new_panel = Panel.new()
+		new_panel.rect_min_size = Vector2(160, 128)
+		new_panel.self_modulate = "00ffffff"
+		$NinePatchRect/GridContainer.add_child(new_panel)
+		var new_char = Character.instance()
+		new_panel.add_child(new_char)
+		var new_label = Label.new()
+		new_label.text = new_char.full_name
+		new_label.ALIGN_CENTER
+		new_label.modulate = "000000"
+		new_panel.add_child(new_label)
+		all.append(new_char)
+	print(all)
